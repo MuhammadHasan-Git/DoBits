e2d867a67f83890e7b4d0ba3009f4011b7c35d15
MuhammadHasan246 <mhasanzaman657@gmail.com>
2024-02-07 02:07:22 +0500

    connect app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/utils/colors.dart';
 import 'package:todo_app/view/home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       systemNavigationBarColor: Colors.transparent,
